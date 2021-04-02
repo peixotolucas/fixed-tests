42ad0e0ff3990bb9699276fc0a96b003d13e98b7
lucas <peixotolucas516@gmail.com>
2021-04-02 12:30:27 -0300

    testes numbers concluidos

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,7 +1,9 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
+const { expect } = require('@jest/globals');
 const assert = require('assert');
+const { number } = require('yargs');
 const numbers = require('../src/numbers');
 
 /*
@@ -18,10 +20,14 @@ const numbers = require('../src/numbers');
 
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
-    assert.fail();
+    
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    expect(numbers([1,2])).toBe(true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    expect(numbers([1, '3', 4, 5])).toBe(false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    expect(numbers([1, 'a', 3])).toBe(false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers([' '])).toBe(false);
   });
 });
