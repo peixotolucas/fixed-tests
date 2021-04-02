27f26d5b91eaaacaf3a83eabd81b795f432ba658
lucas <peixotolucas516@gmail.com>
2021-04-02 12:35:13 -0300

    lint corrections

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -20,9 +20,8 @@ const numbers = require('../src/numbers');
 
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
-    
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    expect(numbers([1,2])).toBe(true);
+    expect(numbers([1, 2])).toBe(true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     expect(numbers([1, '3', 4, 5])).toBe(false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
