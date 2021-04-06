6631d9d3b12d61a8bb8500ab101d120fc33b2a5d
lucas <peixotolucas516@gmail.com>
2021-04-06 19:57:33 -0300

    lint4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -39,8 +39,9 @@ describe('#circle', () => {
     assert.deepStrictEqual((circle(3).area), parseFloat(3.14 * 3 * 3).toPrecision(4));
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.deepStrictEqual((circle(3)),
-      { radius: 3, area: parseFloat(3.14 * 3 * 3).toPrecision(4),
-      circumference: parseFloat(2 * 3.14 * 3).toPrecision(4) });
+      { radius: 3,
+        area: parseFloat(3.14 * 3 * 3).toPrecision(4),
+        circumference: parseFloat(2 * 3.14 * 3).toPrecision(4) });
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
